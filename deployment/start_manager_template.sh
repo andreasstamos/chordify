ca6908c721e4076c2507f23621fd8faf311a228a
Andreas Stamos <stamos.aa@gmail.com>
2025-03-09 22:42:57 +0200

    nginx configuration

--- deployment/start_manager_template.sh
+++ deployment/start_manager_template.sh
@@ -3,8 +3,8 @@
 # Warning! systemd .service file calls me. Edit me with caution at production.
 
 export HAVE_BOOTSTRAP=TRUE
-export BOOTSTRAP_URL="http://127.0.0.1:5000/0"
-export BASE_URL="http://127.0.0.1:5000"
+export BOOTSTRAP_URL="http://10.0.35.58:5000/0"
+export BASE_URL="http://10.0.35.58:5000"
 
 source venv/bin/activate
 gunicorn --workers 1 --worker-class gevent -c gunicorn_manager_conf.py --bind unix:./chordify.sock -m 777 manager:app
